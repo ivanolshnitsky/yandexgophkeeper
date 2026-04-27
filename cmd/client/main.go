4fde3e827a8568948c1d987bc1dd52e38e56fd49
Ivan Olshnitsky <i.olshnitskii@start.ru>
2026-04-27 22:24:45 +0300

    iter5 add in client methods

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,38 +3,78 @@ package main
 import (
 	"fmt"
 	"os"
+	"yandexgophkeeper/internal/config"
 
 	"yandexgophkeeper/internal/client"
-	"yandexgophkeeper/internal/config"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: register|login|add|list")
+		return
+	}
 	cfg := config.New()
 
-	if len(os.Args) < 2 {
-		fmt.Println("usage:")
-		fmt.Println("  ping - check server")
-		return
-	}
+	app := client.New(cfg.ClientURL)
 
-	app := client.New(cfg.BaseURL)
+	switch os.Args[1] {
 
-	cmd := os.Args[1]
+	case "register":
+		if len(os.Args) < 4 {
+			fmt.Println("usage: register <user> <pass>")
+			return
+		}
+		err := app.Register(os.Args[2], os.Args[3])
+		fmt.Println("register:", err)
 
-	switch cmd {
-	case "ping":
-		handlePing(app)
+	case "login":
+		if len(os.Args) < 4 {
+			fmt.Println("usage: login <user> <pass>")
+			return
+		}
+		err := app.Login(os.Args[2], os.Args[3])
+		fmt.Println("login:", err)
+
+	case "add":
+		if len(os.Args) < 5 {
+			fmt.Println("usage: add <type> <value> <meta>")
+			return
+		}
+		err := app.AddData(os.Args[2], os.Args[3], os.Args[4])
+		fmt.Println("add:", err)
+
+	case "list":
+		data, err := app.ListData()
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
+		fmt.Println(data)
+	case "get":
+		if len(os.Args) < 3 {
+			fmt.Println("usage: get <id>")
+			return
+		}
+		res, err := app.GetData(os.Args[2])
+		fmt.Println(res, err)
+
+	case "update":
+		if len(os.Args) < 6 {
+			fmt.Println("usage: update <id> <type> <value> <meta>")
+			return
+		}
+		err := app.UpdateData(os.Args[2], os.Args[3], os.Args[4], os.Args[5])
+		fmt.Println("update:", err)
+
+	case "delete":
+		if len(os.Args) < 3 {
+			fmt.Println("usage: delete <id>")
+			return
+		}
+		err := app.DeleteData(os.Args[2])
+		fmt.Println("delete:", err)
+
 	default:
-		fmt.Println("unknown command:", cmd)
+		fmt.Println("unknown command")
 	}
-}
-
-func handlePing(app *client.App) {
-	resp, err := app.Ping()
-	if err != nil {
-		fmt.Println("error:", err)
-		return
-	}
-
-	fmt.Println(resp)
 }
